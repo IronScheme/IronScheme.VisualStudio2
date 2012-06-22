c2aef8a5fb4b82765a80fe9cc069985eb5087a6a
leppie <xacc.ide@gmail.com>
2012-06-22 22:23:31 +0200

    Take basic logic for REPL from IronPython (err, thanks ;p)

---
+++ IronScheme.VisualStudio.REPL/Guids.cs
@@ -0,0 +1,24 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IronScheme.VisualStudio.REPL
+{
+    public static class ConsoleGuidList
+    {
+        [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly")]
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly")]
+      public const string guidIronSchemeConsolePkgString = "160FCEF2-DED9-4DC2-8E42-985446204ACE";
+
+        [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly")]
+        public const string guidIronSchemeConsoleCmdSetString = "9A3D771E-AA64-4098-AD87-BADA43A16F24";
+
+        [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly")]
+        [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly")]
+        public static readonly Guid guidIronSchemeConsolePkg = new Guid(guidIronSchemeConsolePkgString);
+
+        [SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly")]
+        public static readonly Guid guidIronSchemeConsoleCmdSet = new Guid(guidIronSchemeConsoleCmdSetString);
+    };
+}
---
+++ IronScheme.VisualStudio.REPL/PkgCmdID.cs
@@ -0,0 +1,12 @@
+﻿// MUST match the VSCT file
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IronScheme.VisualStudio.REPL
+{
+    public static class PkgCmdIDList
+    {
+        public const uint ISConsoleContextMenu = 0x2100;
+        public const uint cmdidIronSchemeConsole = 0x101;
+    };
+}
