eeed2d99d0b131c3b7fa7e0d6527f010054a28d8
Llewellyn Pritchard <xacc.ide@gmail.com>
2021-11-17 17:28:41 +0200

    Attempt to build for VS2022

--- IronScheme.VisualStudio/ClassificationTagger.cs
+++ IronScheme.VisualStudio/ClassificationTagger.cs
@@ -123,7 +123,7 @@ namespace IronScheme.VisualStudio
 
       syntax = registry.GetClassificationType(PredefinedClassificationTypeNames.Keyword);
       procedure = registry.GetClassificationType("line number");
-      record = registry.GetClassificationType(PredefinedClassificationTypeNames.Type);
+      record = registry.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition);
 
       _buffer.Properties["SchemeBindings"] = bindings;
 
